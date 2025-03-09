d6f6a5888fd460111b777fdb43d05c2adb790622
Lucas <lucas.sampaio@jala.university>
2025-03-09 19:57:20 -0300

    config

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,10 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(process.env.DATABASE_URL || {
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   dialect: process.env.DB_DIALECT,
   logging: false,
