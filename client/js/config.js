4657abb26a768e0aaa652e2fb949af1f5b5502df
Lucas <lucas.sampaio@jala.university>
2025-03-10 10:16:41 -0300

    config

--- client/js/config.js
+++ client/js/config.js
@@ -1,5 +1,5 @@
 const config = {
-    API_URL: window.API_URL || 'http://localhost:3000', // Usa window.API_URL injetado pelo HTML
+    API_URL: window.API_URL || 'https://uno-deploy-l7eq.onrender.com', 
 };
 
 export default config;
