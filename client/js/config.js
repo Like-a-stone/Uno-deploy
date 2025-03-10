015177a2076c943da0e00680d6c54610a61f05a6
Lucas <lucas.sampaio@jala.university>
2025-03-10 08:40:00 -0300

    config

--- client/js/config.js
+++ client/js/config.js
@@ -1,3 +1,5 @@
 const config = {
-    API_URL: process.env.API_URL || 'http://localhost:3000',
+    API_URL: window.API_URL || 'http://localhost:3000', // Usa window.API_URL injetado pelo HTML
 };
+
+export default config;
